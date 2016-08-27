6e91bdf7924c0b02d1184746701490898459d978
Zakariya Mohamed <zmohamed@eecs.wsu.edu>
2016-08-26 23:20:29 -0700

    NumberedTextWriter set-up. Need more tests.

---
+++ HW1/CS422/NumberedTextWriter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+//using System.IO.TextWriter;
+
+namespace CS422
+{
+	public class NumberedTextWriter : System.IO.TextWriter
+	{
+
+		System.IO.TextWriter wrapper;
+		int currentLineNum = 1 ;
+
+
+		public NumberedTextWriter(System.IO.TextWriter wrapThis)
+		{
+			wrapper = wrapThis;
+
+		}
+
+		public NumberedTextWriter(System.IO.TextWriter wrapThis, int startingLineNumber)
+		{
+			wrapper = wrapThis;
+			currentLineNum = startingLineNumber;
+
+		}
+
+		public override System.Text.Encoding Encoding
+		{
+			get{ return wrapper.Encoding; }
+
+		}
+
+		public override void WriteLine(string value)
+		{
+			string _2prepend =  currentLineNum.ToString()+":"+ " " + value;
+			wrapper.WriteLine(_2prepend);
+			currentLineNum++;
+
+		}
+
+
+	}
+}
---
+++ HW1/TestDriver/Test.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using CS422;
+
+namespace TestDriver
+{
+	[TestFixture ()]
+	public class Test
+	{
+		[Test ()]
+		public void TestCase ()
+		{
+			System.IO.TextWriter x = System.IO.File.CreateText ("/home/zak/Repos/CS422/HW1/testfile.txt");
+
+			CS422.NumberedTextWriter test = new NumberedTextWriter (x,71);
+
+			using (x)
+			{
+				test.WriteLine ("Hello World");
+
+				test.WriteLine ("Hello World");
+
+				test.WriteLine ("Hello World");
+
+				test.WriteLine ("Hello World");
+
+				test.WriteLine ("Hello World");
+			}
+		}
+	}
+}
+
