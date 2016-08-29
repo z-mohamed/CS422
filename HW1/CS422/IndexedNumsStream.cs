0a898c890c6edbf9a175cdd730b4df25d7c7e0a2
Zakariya Mohamed <zmohamed@eecs.wsu.edu>
2016-08-28 20:24:02 -0700

    Implemented I.N.S. Must test thoroughly

--- HW1/CS422/IndexedNumsStream.cs
+++ HW1/CS422/IndexedNumsStream.cs
@@ -7,34 +7,97 @@ namespace CS422
 		
 		byte length;
 
+		long position;
 
-		public IndexedNumsStream (long length)
+
+		public IndexedNumsStream (long lengthIn)
 		{
+			if (lengthIn < 0) 
+			{
+				lengthIn = 0;
+			}
+
+			length = (byte)lengthIn;
+
+			//Why is the setting the position not necessary
+			position = (long)0;
 			
 		}
 
 
-		public override long Length {
-			get {
-				throw new NotImplementedException ();
+		public override long Length 
+		{
+			get 
+			{
+				return length;
+				
 			}
 		}
 
 
 		public override void SetLength (long value)
 		{
-			throw new NotImplementedException ();
+			if (value < 0) 
+			{
+				value = 0;
+			}
+
+			length = (byte)value;
 		}	
 
 
-		public override long Position {
-			get {
-				throw new NotImplementedException ();
+		public override long Position 
+		{
+			get 
+			{
+				return position;
 			}
-			set {
-				throw new NotImplementedException ();
+
+			set 
+			{
+				if (value < 0) 
+				{
+					value = 0;
+				} 
+				else if (value > length) 
+				{
+					value = length;
+
+				}
+
+				position = value;
 			}
 		}
+
+
+		public override int Read (byte[] buffer, int offset, int count)
+		{
+			
+			for (long i = offset; i < count && (byte)position < length; i++)
+			{
+				
+				long value = position % (long)256;
+
+				buffer [i] = (byte) value;
+
+				position++;
+			}
+
+			return count;
+		}
+
+
+		public override void Write (byte[] buffer, int offset, int count)
+		{
+			throw new NotImplementedException ();
+		}
+
+
+		public override long Seek (long offset, System.IO.SeekOrigin origin)
+		{
+			throw new NotImplementedException ();
+		}
+
 
 
 		public override void Flush ()
@@ -67,24 +130,6 @@ namespace CS422
 			{
 				throw new NotImplementedException ();
 			}
-		}
-
-
-		public override int Read (byte[] buffer, int offset, int count)
-		{
-			return 0;
-		}
-
-
-		public override void Write (byte[] buffer, int offset, int count)
-		{
-			throw new NotImplementedException ();
-		}
-
-
-		public override long Seek (long offset, System.IO.SeekOrigin origin)
-		{
-			throw new NotImplementedException ();
 		}
 	}
 }
