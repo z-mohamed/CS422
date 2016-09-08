6182603356b74820cb7cc7e72d9ad04ad461ca59
Zak <zakariya.a.mohamed@gmail.com>
2016-09-08 16:40:08 -0700

    Started HW2

---
+++ HW2/CS422/PCQueue.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CS422
+{
+	public class PCQueue
+	{
+		public PCQueue()
+		{
+			
+		}
+
+		class Node
+		{
+			public object data;
+			public Node next;
+
+			public Node(object val)
+			{
+				
+				data = null;
+				Node.next = null;
+
+			}
+
+		}
+
+
+		public bool Dequeue(ref int out_value)
+		{
+
+			return true;
+		}
+
+
+		public void Enqueue(int dataValue)
+		{
+
+
+		
+		}
+	}
+}
+
