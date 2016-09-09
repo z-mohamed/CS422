b3f4367e370bb852e2440e1639490da87470f6e4
Zakariya Mohamed <zmohamed@eecs.wsu.edu>
2016-09-09 16:08:23 -0700

    Dequeue broken. SleepSort Okay.

--- HW2/CS422/PCQueue.cs
+++ HW2/CS422/PCQueue.cs
@@ -4,40 +4,92 @@ namespace CS422
 {
 	public class PCQueue
 	{
+		public Node _front = null,_back = null,_dummy = null, _temp = null;
+
 		public PCQueue()
 		{
 			
 		}
 
-		class Node
+		public class Node
 		{
-			public object data;
+			public int data;
 			public Node next;
 
-			public Node(object val)
+			public Node(int val)
 			{
 				
-				data = null;
-				Node.next = null;
+				data = val;
+				next = null;
 
 			}
 
 		}
 
+		public void Enqueue(int dataValue)
+		{
+			
+			// empty list
+			if (_front == null) 
+			{
+				_back = new Node (dataValue);
+
+				_front = _back;
+
+			}
+
+			// 1 item in list
+			else if (_front == _back && _front.next == null) 
+			{
+				_back = new Node (dataValue);
+				
+				_front.next = _back;
+			} 
+
+			//>1 item
+			else 
+			{
+				_temp = _back;
+
+				_back = new Node (dataValue);
+
+				_temp.next = _back;
+
+			}
+
+
+		}
+
+
 
 		public bool Dequeue(ref int out_value)
 		{
 
+			//if list is empty, return false
+			if (_front == null && _dummy == null )
+				return false;
+
+			//mark node in front
+			_dummy = _front;
+
+			//if node after front dosen't exist, 1 item in list
+			if (_dummy == null) 
+			{
+				out_value = _front.data;
+				//_front = _;
+
+			} 
+			//multiple item in list delete one in front
+			else 
+			{
+				out_value = _front.data;
+				_front = _dummy;
+				
+			}
+
 			return true;
 		}
-
-
-		public void Enqueue(int dataValue)
-		{
-
-
-		
-		}
+			
 	}
 }
 
---
+++ HW2/CS422/ThreadPoolSleepSorter.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Collections.Concurrent;
+namespace CS422
+{
+	public class ThreadPoolSleepSorter : IDisposable
+	{
+		
+		BlockingCollection<Task> coll =  new BlockingCollection<Task>();
+		TextWriter writer;
+		Thread t;
+
+		//why??
+		bool isDisposed = false;
+		
+		
+		public ThreadPoolSleepSorter (TextWriter output, ushort threadCount)
+		{
+			//set textwriter
+			writer = output;
+
+			//Default thread count = 64
+			if (threadCount == 0)
+				threadCount = 64;
+			
+			// Create number of threads specified by thread count 
+			for (int i = 0; i < threadCount; i++) 
+			{
+				t = new Thread (new ParameterizedThreadStart (ThreadDoWork));
+				t.Start (coll);
+
+			}
+		}
+
+
+		//Take threads from the blocking collection and put them to work
+		public void ThreadDoWork (object _generic_coll)
+		{
+			var _blocking_coll = (BlockingCollection<Task>) _generic_coll;
+
+			while (!coll.IsCompleted) 
+			{
+				try
+				{
+					_blocking_coll.Take().RunTask(writer);
+
+				}
+				catch (Exception ex)
+				{
+					
+
+
+
+
+
+				}
+
+			}
+
+		}
+
+		//for every value in the byte array create a new task and add it to the blocking collection
+		public void Sort(byte[] values)
+		{
+			foreach (byte value in values) 
+			{
+				coll.Add(new Task { _value = value });
+
+			}
+
+		}
+
+		//Sleep for a specified interval then witeline to textwriter
+		public class Task
+		{
+			public byte _value { get; set;}
+
+			public void RunTask(TextWriter writer)
+			{
+				Thread.Sleep (_value * 1000);
+				writer.WriteLine (_value);
+
+
+			}
+				
+		}
+
+		//free all threads
+		public void Dispose()
+		{
+			coll.CompleteAdding();
+			t.Join ();
+
+		}
+	}
+}
+
