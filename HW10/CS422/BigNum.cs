4b908043b80fdbc4fe9631c758395bd314b8134c
Zakariya Mohamed <zmohamed@eecs.wsu.edu>
2016-11-01 23:43:18 -0700

    Progress

--- HW10/CS422/BigNum.cs
+++ HW10/CS422/BigNum.cs
@@ -5,23 +5,107 @@ namespace CS422
 {
 	public class BigNum
 	{
-		BigInteger m_Num;
-		BigInteger m_Power;
+		readonly BigInteger m_Num;
+		readonly int m_Power;
 
 		public BigNum (string number)
 		{
+			// If string does not represent a valid
+			// number throw an exception
+			if (!IsStringValidNum (number))
+				throw new ArgumentException ();
+
+
+			// Remove all insignificant zeros
+			// that are left of decimal point
+			number = PruneLeadingZeros(number);
+
+
+			if(number.Contains("."))
+			{
+				// Remove all insignificant zeros
+				// that are right of decimal point
+				number = PruneTrailingZeros (number);
+
+
+				int decimalIndex = number.IndexOf ('.');
+
+				// Remove decimal index
+				number= number.Remove (decimalIndex, 1);
+
+				m_Power = (number.Length - decimalIndex);
+			}
+			else
+			{
+				m_Power = 0;
+			}
+
+			m_Num = BigInteger.Parse (number);
+
+			isUndefined = false;
+			
 		}
 
 		public BigNum( double value, bool useDoubleToString)
 		{
+			if(IsDoubleUndefined(value))
+			{
+				isUndefined = true;
+			}
+			else if(useDoubleToString)
+			{
+				string number = value.ToString ();
+
+				// Redundant code
+				if (!IsStringValidNum (number))
+					throw new ArgumentException ();
+
+				number = PruneLeadingZeros(number);
+
+
+				if(number.Contains("."))
+				{
+					number = PruneTrailingZeros (number);
+
+					int decimalIndex = number.IndexOf ('.');
+					number= number.Remove (decimalIndex, 1);
+					m_Power = (number.Length - decimalIndex);
+				}
+				else
+				{
+					m_Power = 0;
+				}
+
+				m_Num = BigInteger.Parse (number);
+
+				isUndefined = false;
+
+			}
+			else
+			{
+				//Parse through the bits in value
+			}
 		}
 			
 		public override string ToString ()
 		{
-			return string.Format ("[BigNum]");
+			if (this.isUndefined) 
+			{
+				return "undefined";
+			}
+
+			string num = m_Num.ToString ();
+
+			if (m_Power != 0) 
+			{
+				int decimalIndex = num.Length - m_Power;
+				num = num.Insert (decimalIndex, ".");
+			}
+			return num;
 		}
 
 		public bool isUndefined { get;}
+
 
 		public static BigNum operator+(BigNum lhs, BigNum rhs)
 		{
@@ -72,17 +156,85 @@ namespace CS422
 		/*				Utility Functions 				   */
 		/***************************************************/
 
-		internal bool IsStringValidNum(string number)
+		public static bool IsStringValidNum(string number)
 		{
-			throw new NotImplementedException ();
+			if (number == null)
+				return false;
+			
+			if(number.Contains(" ")|| number == "" )
+				return false;
+
+			int countDecimalPoint = 0;
+			int countMinusSign = 0;
+
+			for(int i = 0; i <number.Length; i++)
+			{
+				if (number [i] != '.' && number [i] != '-' && !Char.IsDigit (number [i]))
+					return false;
+				
+				if (number [i] == '-' && i != 0)
+					return false;
+
+				if (number [i] == '-')
+					countMinusSign++;
+				
+				if (number [i] == '.')
+					countDecimalPoint++;
+
+				if (countDecimalPoint > 1 || countMinusSign > 1)
+					return false;
+			}
+
+			return true;
 		}
 
-		internal bool IsDoubleUndefined(double value)
+		public static bool IsDoubleUndefined(double value)
 		{
-			throw new NotImplementedException ();
+			if (Double.IsNaN (value) || Double.IsInfinity (value))
+				return true;
+
+			return false;
+		}
+
+		public static string PruneLeadingZeros (string number)
+		{
+			string leanNum;
+			int i = 0;
+
+			while (number [i] == '0' && i<number.Length) 
+			{
+				i++;
+			}
+
+			if (i == number.Length)
+				return "0";
+		
+			leanNum = number.Substring (i);
+
+			if (leanNum == ".")
+				leanNum = "0";
+
+
+			return leanNum;
+		}
+
+		public static string PruneTrailingZeros (string number)
+		{
+			string leanNum;
+			int i = number.Length - 1;
+
+			while (number [i] == '0') 
+			{
+				i--;
+			}
+
+			leanNum = number.Substring (0, i + 1);
+
+			if (leanNum == ".")
+				leanNum = "0";
+			
+			return leanNum;
 		}
 
 	}
-
-
 }
---
+++ HW10/UnitTests/Test.cs
@@ -0,0 +1,108 @@
+﻿using NUnit.Framework;
+using System;
+using CS422;
+
+namespace UnitTests
+{
+	[TestFixture ()]
+	public class Test
+	{
+		[Test ()]
+		public void IsStringValidNum ()
+		{
+			Assert.False (CS422.BigNum.IsStringValidNum ("    "));
+			Assert.False (CS422.BigNum.IsStringValidNum ("-1234-5"));
+			Assert.False (CS422.BigNum.IsStringValidNum ("hello"));
+			Assert.False (CS422.BigNum.IsStringValidNum ("-12345.789 "));
+			Assert.False (CS422.BigNum.IsStringValidNum ("12234-"));
+			Assert.False (CS422.BigNum.IsStringValidNum ("122-34"));
+			Assert.False (CS422.BigNum.IsStringValidNum ("12.23.4"));
+			Assert.False (CS422.BigNum.IsStringValidNum ("s12356"));
+			Assert.False (CS422.BigNum.IsStringValidNum ("12a234"));
+			Assert.False (CS422.BigNum.IsStringValidNum (""));
+			Assert.False (CS422.BigNum.IsStringValidNum (null));
+
+			Assert.True (CS422.BigNum.IsStringValidNum ("123455"));
+			Assert.True (CS422.BigNum.IsStringValidNum ("-123455"));
+			Assert.True (CS422.BigNum.IsStringValidNum ("-12345.5"));
+			Assert.True (CS422.BigNum.IsStringValidNum ("-.123455"));
+			Assert.True (CS422.BigNum.IsStringValidNum (".123455"));
+			Assert.True (CS422.BigNum.IsStringValidNum ("123455."));
+		}
+
+		[Test ()]
+		public void StandardCtor ()
+		{
+			BigNum myBigNum = new BigNum ("127.6448");
+			Assert.False (myBigNum.isUndefined);
+		}
+			
+		[Test ()]
+		public void PruneLeadingZeros  ()
+		{
+			string pruned = BigNum.PruneLeadingZeros ("0.07");
+			Assert.AreEqual (".07", pruned);
+
+			pruned = BigNum.PruneLeadingZeros ("000000010.07");
+			Assert.AreEqual ("10.07", pruned);
+		}
+
+		[Test ()]
+		public void PruneTrailingZeros  ()
+		{
+			string pruned = BigNum.PruneTrailingZeros ("0.789000001");
+			Assert.AreEqual ("0.789000001", pruned);
+
+			pruned = BigNum.PruneTrailingZeros ("0.78900000");
+			Assert.AreEqual ("0.789", pruned);
+		}
+			
+		[Test ()]
+		public void PruneEverything  ()
+		{
+			string pruned = BigNum.PruneTrailingZeros ("0.789000001");
+			pruned = BigNum.PruneLeadingZeros (pruned);
+			Assert.AreEqual (".789000001", pruned);
+
+			pruned = BigNum.PruneTrailingZeros ("000000010.78900000");
+			pruned = BigNum.PruneLeadingZeros (pruned);
+			Assert.AreEqual ("10.789", pruned);
+		}
+
+		[Test ()]
+		public void PruneZero  ()
+		{
+			string pruned = BigNum.PruneTrailingZeros ("00000000000000000.0000000000000000");
+			pruned = BigNum.PruneLeadingZeros (pruned);
+			Assert.AreEqual ("0", pruned);
+		}
+
+		[Test ()]
+		public void ToStringSimple  ()
+		{
+			BigNum myBigNum = new BigNum ("000127.64480000");
+			string num = myBigNum.ToString();
+			Assert.AreEqual ("127.6448", num);
+
+			BigNum myBigNum2 = new BigNum ("00012764480000");
+			string num2 = myBigNum2.ToString();
+			Assert.AreEqual ("12764480000", num2);
+
+			BigNum myBigNum3 = new BigNum ("-00012764480000");
+			string num3 = myBigNum3.ToString();
+			Assert.AreEqual ("-12764480000", num3);
+
+			BigNum myBigNum4 = new BigNum ("0.40");
+			string num4 = myBigNum4.ToString();
+			Assert.AreEqual (".4", num4);
+		}
+
+		/*
+		[Test ()]
+		public void  ()
+		{
+		}
+		*/
+	}
+}
+
