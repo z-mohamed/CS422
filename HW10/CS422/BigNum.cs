b0bced0fcd1717ce6aad8c06d72ffee98ae68f94
Zak <zakariya.a.mohamed@gmail.com>
2016-11-01 19:41:13 -0700

    Begin

---
+++ HW10/CS422/BigNum.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Numerics;
+
+namespace CS422
+{
+	public class BigNum
+	{
+		BigInteger m_Num;
+		BigInteger m_Power;
+
+		public BigNum (string number)
+		{
+		}
+
+		public BigNum( double value, bool useDoubleToString)
+		{
+		}
+			
+		public override string ToString ()
+		{
+			return string.Format ("[BigNum]");
+		}
+
+		public bool isUndefined { get;}
+
+		public static BigNum operator+(BigNum lhs, BigNum rhs)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public static BigNum operator-(BigNum lhs, BigNum rhs)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public static BigNum operator*(BigNum lhs, BigNum rhs)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public static BigNum operator/(BigNum lhs, BigNum rhs)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public static bool operator>(BigNum lhs, BigNum rhs)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public static bool operator>=(BigNum lhs, BigNum rhs)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public static bool operator<(BigNum lhs, BigNum rhs)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public static bool operator<=(BigNum lhs, BigNum rhs)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public static bool IsToStringCorrect(double value)
+		{
+			throw new NotImplementedException ();
+		}
+
+		/***************************************************/
+		/*				Utility Functions 				   */
+		/***************************************************/
+
+		internal bool IsStringValidNum(string number)
+		{
+			throw new NotImplementedException ();
+		}
+
+		internal bool IsDoubleUndefined(double value)
+		{
+			throw new NotImplementedException ();
+		}
+
+	}
+
+
+}
